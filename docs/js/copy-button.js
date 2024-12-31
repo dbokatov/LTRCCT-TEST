18d3723b2d9615e2bd8c15f158dcba2dfb8391c6
Dimitri Bokatov <d.bokatov@gmail.com>
2024-12-31 13:00:23 +0100

    Update copy-button.js

--- docs/js/copy-button.js
+++ docs/js/copy-button.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const copyButtons = document.querySelectorAll(".copy-button");
-    copyButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            const textToCopy = button.getAttribute("data-copy-text");
+    const copyIcons = document.querySelectorAll(".copy-icon");
+    copyIcons.forEach(icon => {
+        icon.addEventListener("click", function() {
+            const textToCopy = icon.getAttribute("data-copy-text");
             navigator.clipboard.writeText(textToCopy).then(() => {
                 console.log("Text copied to clipboard");
             }).catch(err => {
